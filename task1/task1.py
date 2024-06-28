3d69c8e5c806f4bfad7a043ec824871ec736213c
viktorice <vamorozov01@outlook.com>
2024-06-28 16:20:51 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,20 +1,20 @@
 def circular_array(array_length, length_interval):
-    num = ''
-    cum = ''
+    num_1 = ''
+    num_2 = ''
     x = 0
     y = 0
     for i in range(1, array_length + 1):
-        num += str(i)
-    num = num * length_interval * 2
+        num_1 += str(i)
+    num_1 = num_1 * length_interval * 2
     for k in range(length_interval * 2):
-        for j in num[:length_interval]:
-            cum += j
+        for j in num_1[:length_interval]:
+            num_2 += j
             x += 1
             if x == length_interval:
-                print(cum[length_interval * y], end='')
+                print(num_2[length_interval * y], end='')
                 y += 1
                 x = 0
-                num = num[length_interval - 1:]
+                num_1 = num_1[length_interval - 1:]
         if j == '1':
             break
 
